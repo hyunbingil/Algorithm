a99310ecb64501a82d6f81bd5be091d1145821a5
hyunbingil <super199g@naver.com>
2020-08-05 17:30:45 +0900

    🎈 1070

---
+++ CodeUp/1063 - 1072/1067.py
@@ -0,0 +1,11 @@
+n = input()
+n = int(n)
+if n < 0:
+    print('minus')
+elif n > 0:
+    print('plus')
+
+if n % 2 == 0:
+    print('even')
+else:
+    print('odd')    
---
+++ CodeUp/1063 - 1072/1068.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if (n >= 90) and (n <= 100):
+    print("A")
+elif (n >= 70) and (n <= 89):
+    print("B")
+elif (n >= 40) and (n <= 69):
+    print("C")
+elif (n >= 0) and (n <= 39):
+    print("D")           
